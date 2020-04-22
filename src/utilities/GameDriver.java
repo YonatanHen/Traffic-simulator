f158f944753a98865dc17b733585bb4c155c16cc
Yehonatan Hen <yehone@ac.sce.ac.il>
2020-04-22 16:22:03 +0300

    Implement utilities interface and initial classes/enums

---
+++ src/utilities/GameDriver.java
@@ -0,0 +1,12 @@
+//Yehonatan Hen-207630112
+//Rotem Librati-
+
+package utilities;
+
+public class GameDriver {
+    public static void main(String[] args) {
+        Driving driving=new Driving(10, 20);
+        driving.drive(20);
+    }
+
+}
