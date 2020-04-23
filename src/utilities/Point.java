a1a1f91088ebde2f67a5fdcbfa13c3ae7a4391cf
Yehonatan Hen <yehone@ac.sce.ac.il>
2020-04-23 10:43:31 +0300

    class point

--- src/utilities/Point.java
+++ src/utilities/Point.java
@@ -1,27 +1,68 @@
 package utilities;
 
+
+/**
+ * class represent a point on the map.
+ *
+ * @author Yehonatan Hen-207630112
+ * @author Rotem Librati-
+ * @see Utilities
+ */
 public abstract class Point implements Utilities {
     final int minVal=0;//Minimal values of coordinates X and Y
     final int maxX=800;//Maximal value of X
     final int maxY=600;//Maximal value of Y
     //x and y are coordinates of some point on the map in cartesian axis system
-    int x;
-    int y;
+    double x;
+    double y;
 
+    /**
+     * Point constructor
+     *
+     * @param x
+     * @param y
+     */
     Point(double x,double y){
-        //TODO:check if we need to import x and y from double to int
-        if(checkValue(x,minVal,maxX)) this.x=(int)x;
+        if(checkValue(x,minVal,maxX)) this.x=x;
         else {
            double wrongVal=x;
            x=getRandomDouble(minVal,maxX);
            correctingMessage(wrongVal,x,"x");
         }
-        if(checkValue(y,minVal,maxY)) this.y=(int)y;
+        if(checkValue(y,minVal,maxY)) this.y=y;
         else {
             double wrongVal=y;
             x=getRandomDouble(minVal,maxY);
             correctingMessage(wrongVal,y,"y");
         }
-        successMessage(this.toString());
+        successMessage(toString());
     }
+
+    /**
+     * Point constructor-make point with randomal values
+     */
+    Point(){
+        x=getRandomDouble(minVal,maxX);
+        y=getRandomDouble(minVal,maxY);
+        successMessage(toString());
+    }
+
+    //getters
+    public double getX(){return x;}
+    public double getY(){return y;}
+    //setters
+    public void setX(double x){this.x=x;}
+    public void setY(double y){this.y=y;}
+
+    public String toString(){
+        return "Point ("+x+" , "+y+")";
+    }
+
+    public boolean equals(Object o){
+        if (o instanceof Point){
+            return (((Point) o).x==x && ((Point)o).y==y);
+        }
+        return false;
+    }
+
 }
--- src/utilities/Utilities.java
+++ src/utilities/Utilities.java
@@ -1,49 +1,95 @@
-//Yehonatan Hen-207630112
-//Rotem Librati-
 
 package utilities;
 
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * @author Yehonatan Hen-207630112
+ * @author Rotem Librati-
+ */
 public interface Utilities {
+    /**
+     * function receive a value and check if it is inside the legal range.
+     *
+     * @param val
+     * @param min
+     * @param max
+     * @return true if value is legal,else false
+     */
     default boolean checkValue(double val,double min,double max) {
-    //Function return true if val value is legal
        return val<=max && val>=min;
     }
 
+    /**
+     * Function print message of changing variable value to the correct one.
+     *
+     * @param wrongVal
+     * @param correctVal
+     * @param varName
+     */
     default void correctingMessage(double wrongVal, double correctVal, String varName){
-    //Function print message of changing variable value to the correct one.
-        //TODO:Find if this implementation match the test output as demand.
-        System.out.println("Value of "+ varName+" has been changed from "+ wrongVal+" to "+correctVal+".");
+        System.out.println("The Value of "+ wrongVal +" is illegal for "+ varName+", therefore has been replaced with "+correctVal);
     }
 
+    /**
+     * Function print a message that shows if the value is illegal
+     *
+     * @param wrongVal
+     * @param varName
+     */
     default  void errorMessage(double wrongVal, String varName){
-    //Function print a message that shows if the value is illegal
         System.out.println("Value "+ wrongVal+ " of " + varName+"is illegal.");
     }
 
+
+    /**
+     * function randomise a boolean value and return him.
+     *
+     * @return randomal boolean value
+     */
     default boolean getRandomBoolean(){
-    //Function return randomal boolean
         Random rand=new Random();
         return rand.nextBoolean();
     }
 
+    /**
+     * Function return random value in the range between min and max.
+     *
+     * @param min
+     * @param max
+     * @return randomal double
+     */
     default double getRandomDouble(double min, double max){
-    //Function return random value in the range between min and max
         Random rand=new Random();
         return (rand.nextDouble()+rand.nextInt((int)max)+min);
     }
 
+    /**
+     * Function randomise an integer value in the range of min-max
+     * and return him.
+     *
+     * @param min
+     * @param max
+     * @return Integer value in the range of min-max
+     */
     default int getRandomInt(int min, int max){
         //TODO:Why double in the instructions? it suppose to be int
-    //Function return integer value in the range of min-max
+
         Random rand=new Random();
         return rand.nextInt(max)+min;
     }
 
+    /**
+     * Function return an array list of randomal integers between min and max
+     *
+     * @param min
+     * @param max
+     * @param arraySize
+     * @retrn Randomal array list of integers
+     */
     default ArrayList<Integer> getRandomIntArray(int min, int max, int arraySize){
-    //Function return an array list of randomal integers between min and max
+
         ArrayList<Integer> arrList=new ArrayList<>();
         for(int i = 0; i<arraySize; i++){
             arrList.add(getRandomInt(min,max));
@@ -51,8 +97,12 @@ public interface Utilities {
         return arrList;
     }
 
+    /**
+     * Function prints success message when an object created successfully.
+     *
+     * @param objName
+     */
     default void successMessage(String objName){
-    //Function prints a message of successful creation of object
-        System.out.println(objName+" created successfully.");
+        System.out.println(objName+" has been created.");
     }
 }
